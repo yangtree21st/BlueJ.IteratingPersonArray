e79aa59d63a79f3336c91d82b60b78c6610710fd
yang shu <yangtree21st@gmail.com>
2019-02-11 17:11:17 -0500

    finish and submit

--- PersonHandler.java
+++ PersonHandler.java
@@ -1,4 +1,4 @@
-package com.zipcodewilmington;
+ 
 
 /**
  * Created by leon on 1/24/18.
@@ -12,10 +12,16 @@ public class PersonHandler {
 
     public String whileLoop() {
         String result = "";
+        
         // assume there is a `counter`
+        int counter = 0;
         // while `counter` is less than length of array
+         while (counter < personArray.length) {
+            result += personArray[counter].toString();
+            counter ++;
+        }
             // begin loop
-
+            
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
@@ -31,16 +37,18 @@ public class PersonHandler {
         // identify initial value
         // identify terminal condition
         // identify increment
-
+        for (int i = 0; i < personArray.length; i ++) {
         // use the above clauses to declare for-loop signature
             // begin loop
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
             // end loop
-
-        return result;
-    }
+                result += personArray[i].toString();
+        
+            }
+              return result;
+            }
 
 
 
@@ -48,13 +56,14 @@ public class PersonHandler {
         String result = "";
         // identify array's type
         // identify array's variable-name
-
+        for (Person currentPerson: personArray) {
         // use the above discoveries to declare for-each-loop signature
             // begin loop
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
             // end loop
-
+             result += currentPerson.toString();}
+             
         return result;
     }
 
